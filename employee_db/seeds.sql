398f297a97bab8824df57ee45f5633b061afcd87
mbenson025 <mbenson025@gmail.com>
2022-06-20 23:49:49 -0500

    finished schema and seeds

--- employee_db/seeds.sql
+++ employee_db/seeds.sql
@@ -1,22 +1,27 @@
-INSERT INTO employee (first_name, last_name, id, role_id, manager_id)
-VALUES ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", );
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Paul", "Allen", 1, 1),
+      ("Patrick", "Bateman", 1, 2),
+      ("Timothy", "Bryce", 4, 3),
+      ("Donald", "Kimball", 5, 4),
+      ("Harold", "Cranes", 2, 5),
+      ("Luis", "Carruthers", 3, null),
+      ("Craig", "McDermott", 4, null),
+      ("Evelyn", "Williams", 5, null),
+      ("David", "VanPatten", 2, null),
+      ("Courtney", "Rawlinson", 3, null);
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", ),
-       ("", "", );
+INSERT INTO roles (title, salary, department_id)
+VALUES ( "Executive Manager", 600000, 1 ),
+       ( "Vice President", 450000, 1 ),
+       ( "Engineer", 250000, 6),
+       ( "Assistant", 60000, 3),
+       ( "Developer", 90000, 7);
 
-INSERT INTO department (id, department_name)
-VALUES (1, "Marketing"),
-       (2, "Finance"),
-       (3, "Human Resources"),
-       (4, "Research"),
-       (4, "Quality Management"),
-       (4, "Production"),
-       (5, "Development");
+INSERT INTO department (name)
+VALUES ( "Mergers and Aquisitions"),
+       ( "Finance"),
+       ( "Human Resources"),
+       ( "Research"),
+       ( "Quality Management"),
+       ( "Production"),
+       ( "Development");
